c0bb47a6465e2601005a913e28937a07c0ae8a3e
Giovanni Fernandes <giofcosta@gmail.com>
2018-10-24 00:06:41 -0300

    publish backend

---
+++ backend/src/config/routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const todoService = require('../api/todo/todoService')
+
+module.exports = function (server) {
+    const router = express.Router()
+    server.use('/api', router)
+    
+    todoService.register(router, '/todos')
+ }
